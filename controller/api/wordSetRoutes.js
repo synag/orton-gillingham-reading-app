3bb94b10edb5eb168c5a76680e46c0a78bc6b6b1
Carl Benbow <cbenbow@synergisticagility.com>
2022-05-21 12:12:21 -0400

    added an htmlRoute files

--- controller/api/wordSetRoutes.js
+++ controller/api/wordSetRoutes.js
@@ -1,1 +1,14 @@
 const router = require('express').Router();
+
+
+
+
+
+//get array of works - ight 
+// get array of vowels
+// get array consensant
+// site word
+
+
+
+//Create new words to the list
